1be0c479543f2154b1e9d637974d9a9a111ee66f
Mingshi Peng <pengms0827@gmail.com>
2016-12-22 16:03:52 -0500

    new pre-trained model added
    Add model_starrynight.t7

--- trained_model/download_trained_model.sh
+++ trained_model/download_trained_model.sh
@@ -5,6 +5,8 @@ wget http://cs.nyu.edu/~mp4504/model_wave.t7
 wget http://cs.nyu.edu/~mp4504/model_the_scream.t7
 wget http://cs.nyu.edu/~mp4504/model_mosaic.t7
 wget http://cs.nyu.edu/~mp4504/model_feathers.t7
+wget http://cs.nyu.edu/~mp4504/model_starrynight.t7
+
 
 
 
