fb4ba8767725c98abb3c051687b8199f987cd4b9
Mingshi Peng <pengms0827@gmail.com>
2016-12-23 11:51:57 -0500

    Update download_trained_model.sh

--- trained_model/download_trained_model.sh
+++ trained_model/download_trained_model.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-wget http://cs.nyu.edu/~mp4504/model_la_muse.t7
-wget http://cs.nyu.edu/~mp4504/model_wave.t7
-wget http://cs.nyu.edu/~mp4504/model_the_scream.t7
-wget http://cs.nyu.edu/~mp4504/model_mosaic.t7
-wget http://cs.nyu.edu/~mp4504/model_feathers.t7
-wget http://cs.nyu.edu/~mp4504/model_starrynight.t7
+wget http://cs.nyu.edu/~mp4504/storage/model_la_muse.t7
+wget http://cs.nyu.edu/~mp4504/storage/model_wave.t7
+wget http://cs.nyu.edu/~mp4504/storage/model_the_scream.t7
+wget http://cs.nyu.edu/~mp4504/storage/model_mosaic.t7
+wget http://cs.nyu.edu/~mp4504/storage/model_feathers.t7
+wget http://cs.nyu.edu/~mp4504/storage/model_starrynight.t7
 
 
 
